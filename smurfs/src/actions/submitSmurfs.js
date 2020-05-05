cfc6fd5f67d3b625a22285ea716659d5ac99d2a1
Kristin Barr <kristinbarr@gmail.com>
2020-05-05 13:46:37 -0400

    cleaned up submit smurfs

--- smurfs/src/actions/submitSmurfs.js
+++ smurfs/src/actions/submitSmurfs.js
@@ -5,20 +5,17 @@ export const SUBMIT_SMURF_START = 'SUBMIT_SMURF_START'
 export const SUBMIT_SMURF_SUCCESS = 'SUBMIT_SMURF_SUCCESS'
 export const SUBMIT_SMURF_FAILURE = 'SUBMIT_SMURF_FAILURE'
 
-// THUNK contains asynchronous function and dispatches objects
-export const submitSmurf = (smurf) => dispatch => {
-  return (dispatch) => {
+// THUNK contains an asynchronous function and dispatches objects
+export const submitSmurf = smurf => {
+  return dispatch => {
     dispatch({ type: SUBMIT_SMURF_START })
     axios
-      .post(
-        'http://localhost:3333/smurfs', smurf)
-      .then((res) => {
-        console.log('res',res)
-        dispatch({ type: SUBMIT_SMURF_SUCCESS, payload: res.data.data })
+      .post('http://localhost:3333/smurfs', smurf)
+      .then(res => {
+        dispatch({ type: SUBMIT_SMURF_SUCCESS, payload: res.data })
       })
-      .catch((err) => {
+      .catch(err => {
         dispatch({ type: SUBMIT_SMURF_FAILURE, payload: err.response })
       })
   }
 }
-
