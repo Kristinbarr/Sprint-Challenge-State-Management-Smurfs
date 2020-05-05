3b351efc20ee703602f3af8530becd1bc1405db1
Kristin Barr <kristinbarr@gmail.com>
2020-05-05 13:46:47 -0400

    added delete smurfs functionality

---
+++ smurfs/src/actions/deleteSmurf.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+// ACTION TYPES
+export const DELETE_SMURF_START = 'DELETE_SMURF_START'
+export const DELETE_SMURF_SUCCESS = 'DELETE_SMURF_SUCCESS'
+export const DELETE_SMURF_FAILURE = 'DELETE_SMURF_FAILURE'
+
+export const deleteSmurf = smurf => {
+  console.log('deleted smurf:',smurf)
+  return dispatch => {
+    dispatch({ type: DELETE_SMURF_START })
+    axios
+      .delete(`http://localhost:3333/smurfs/${smurf.id}`)
+      .then(res => {
+        dispatch({ type: DELETE_SMURF_SUCCESS, payload: res.data })
+      })
+      .catch(err =>
+        dispatch({ type: DELETE_SMURF_FAILURE, payload: err.response })
+      )
+  }
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,47 +1,77 @@
 import {
-  getSmurfs,
   SUBMIT_SMURF_START,
   SUBMIT_SMURF_SUCCESS,
+  SUBMIT_SMURF_FAILURE,
   FETCH_SMURFS_START,
-  FETCH_SMURFS_SUCCESS
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_FAILURE,
+  DELETE_SMURF_START,
+  DELETE_SMURF_SUCCESS,
+  DELETE_SMURF_FAILURE,
 } from '../actions'
 
 export const initialState = {
   smurfs: [],
-  fetchingSmurfs: false,
-  isSubmitting: false,
+  isLoading: false,
   error: ''
 }
 
 export const reducer = (state = initialState, action) => {
+  console.log('ACTION.TYPE:', action.type, 'ACTION.PAYLOAD:', action.payload)
   switch (action.type) {
+    // SUBMIT SMURF
     case SUBMIT_SMURF_START:
       return {
         ...state,
-        smurfs: [],
-        isSubmitting: true,
-        error: ''
+        isLoading: true,
       }
     case SUBMIT_SMURF_SUCCESS:
       return {
         ...state,
-        smurfs: [...state.smurfs, action.payload],
-        isSubmitting: false,
-        error: ''
+        smurfs: [action.payload],
+        isLoading: false,
       }
+    case SUBMIT_SMURF_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: 'Error submitting smurf!'
+      }
+    // FETCH SMURFS
     case FETCH_SMURFS_START:
       return {
         ...state,
-        smurfs: [],
-        fetchingSmurfs: true,
-        error: ''
+        isLoading: true,
       }
     case FETCH_SMURFS_SUCCESS:
       return {
         ...state,
         smurfs: action.payload,
-        fetchingSmurfs: false,
-        error: ''
+        isLoading: false,
+      }
+    case FETCH_SMURFS_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: 'Error fetching Smurf!'
+      }
+    // DELETE SMURF
+    case DELETE_SMURF_START:
+      return {
+        ...state,
+        isLoading: true,
+      }
+    case DELETE_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: [...state.smurfs, action.payload],
+        isLoading: false,
+      }
+    case DELETE_SMURF_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: 'Error deleting smurf!'
       }
     default:
       return state
